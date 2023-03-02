a9b54961a98f3fac6dd3b81d4058df1bcea27198
JJH <jakebbak@gmail.com>
2023-03-02 23:37:38 +0900

    소수 찾기

--- src/Level1/find_primeNumber.java
+++ src/Level1/find_primeNumber.java
@@ -1,4 +1,23 @@
 package Level1;
 
+
+import java.util.ArrayList;
+
 public class find_primeNumber {
+    public int solution(int n) {
+        ArrayList<Integer> list = new ArrayList<>();
+        boolean[] flag = new boolean[n+1]; //false로 초기화
+
+        //에라토스테네스의 체
+        for (int i = 2; i <= n; i++) {
+            if (!flag[i]) {
+                list.add(i);
+                for (int j = 2 * i; j <= n; j += i) {
+                    flag[j] = true;
+                }
+            }
+        }
+
+        return list.size();
+    }
 }
